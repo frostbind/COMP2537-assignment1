1aeade673ea7ce9ac25197c2a188b6010052af9b
Alexander Farlinger <alexanderfarlinger@gmail.com>
2022-05-11 22:08:28 -0700

    install mongoose commit
    I don't even know what half these files do anyways

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const app = express()
 const https = require("https");
+const mongoose = require("mongoose");
 app.set('view engine', 'ejs');
 
 app.listen(5000, function (err) {
     if (err)
         console.log(err);
-})   
+})  
 
-// app.get('/', function (req, res) {
-//     res.send('<h1> GET request to homepage </h1>')    
-// })
+mongoose.connect("mongodb+srv://frostbind:Alex1427@cluster0.5wm77.mongodb.net/assignment2?retryWrites=true&w=majority",
+                        {
+                            // useNewUrlParser: true,
+                            // useUnifiedtepology: true,
+                        });
 
 app.get('/profile/:id', function (req, res) {
     console.log(req.params);
@@ -47,10 +50,4 @@ app.get("/search", function (req, res) {
     res.redirect("search.html")
 })
 
-// app.get('/', function(req, res) {
-//     res.sendFile(__dirname + "/index.html");
-//   })
-
 app.use(express.static('./public'));
-
-
