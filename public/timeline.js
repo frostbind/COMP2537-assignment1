fdba3a41339650f352f3b2982c9f6539a6220f77
Alexander Farlinger <alexanderfarlinger@gmail.com>
2022-05-13 22:22:01 -0700

    timeline insert and read works

--- public/timeline.js
+++ public/timeline.js
@@ -1,16 +1,53 @@
-const { default: mongoose } = require("mongoose")
+to_add = ``;
+length = 0;
 
-function post() {
-    mongoose.post("")
+async function increaseHits(id) {
+
+    console.log("click")
+
+    type = `http://localhost:5000/api/update`
+    await $.ajax({
+        type: "GET",
+        url: type,
+        data: {
+            thing: id,
+        },
+        success: function () {
+            
+        }
+    })
 }
 
+// async function post() {
+//     type = `http://localhost:5000/api/testMongo`
+//     console.log(type)
+//     await $.ajax({
+//         type: "POST",
+//         url: type,
+//     })
+// }
 
 function print(data) {
     console.log(data)
+    to_add = ``;
+    
+    to_add += `<div id="timelineContainer">`
+
+    for (let i = 0; i < data.length; i++) {
+        to_add += `<div style="border: solid 1px black;">`
+        to_add += `<div>${new Date(data[i].event.date)}</div>`
+        to_add += `<div>${data[i].event.hits}</div>`
+        to_add += `<button id="${data[i]._id}" class="timelineButton">like</button>`
+        to_add += `<span style="display: none;">${data[i]._id}</span>`
+        to_add += `</div>`
+        length++;
+    }
+
+    jQuery("main").html(to_add)
 }
 
 async function setup() {
-    type = `http://localhost:5000/api/test`
+    type = `http://localhost:5000/api/testEvent`
     console.log(type)
     await $.ajax({
         type: "GET",
--- server/routes/testMongo.js
+++ server/routes/testMongo.js
@@ -3,32 +3,46 @@ const router = express.Router();
 const pokemon = require('./pokemon.json');
 const mongoose = require("mongoose");
 const mongodb = "mongodb+srv://frostbind:Alex1427@cluster0.5wm77.mongodb.net/assignment2?retryWrites=true&w=majority";
+let eventDb
+mongoose.connect(mongodb, function (err, db) {
+    if (err) {throw err;}
+    eventDb = db;
+})
 
+router.get ("/api/update", (req, res) => {
+  mongoose.connect(mongodb, function (err, db) {
+    db.collection('Events').update(
+      {hits: {$eq: req.thing}},
+      {$set: {hits: hits+1}},
+      {multi: true})
+  })
+})
 router.get("/api/testMongo", (req, res) => {
     mongoose.connect(mongodb, function (err, db) {
-        // console.log(db.collection("Pokemon").find({}));
-
-        db.collection('Pokemon').insertOne({
-            "charmander": {
-                "name": "Charmander2",
-                "id":   "",
-                "atk":  "",
-                "def":  "",
-                "sAtk": "",
-                "sDef": "",
-                "spd":  ""
+        db.collection('Events').insertOne({
+            "event": {
+                "date": Date.now(),
+                "hits": 0,
             }
           })
           .then(function(result) {
             // process result
           })
     })
-
-    // res.set("Access-Control-Allow-Origin", "*");    
-    // res.set("Access-Control-Allow-Methods", "*");
-    // res.set("Content-Type", "application/json");
-    //res.status(200).json(pokemon);
-    res.status(200).send("hello world")
+    res.redirect("./../timeline.html")
 });
+
+router.get("/api/testEvent", (req, res) => {
+  mongoose.connect(mongodb, function (err, db) {
+    db.collection('Events').find().toArray(function (err, result) {
+      if (result.length < 1) {
+        console.log("No results")
+      } else {
+        res.status(200).send(result)
+      }
+    });
+    })
+    
+})
 
 module.exports = router;
